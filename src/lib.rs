7f23ffe138caa6217299b9f066bbc9fb77dc88c2
flatypus [mac] <yhc3141@gmail.com>
2023-11-02 14:30:47 -0700

    feat: fetch and cache from overpass

---
+++ src/lib.rs
@@ -0,0 +1,80 @@
+use serde_json::{json, Value};
+use std::collections::HashMap;
+
+pub async fn grab_city_data(city: &str) -> Result<Vec<HashMap<&str, Value>>, ()> {
+    let client = reqwest::Client::new();
+    let url = format!(
+        "https://nominatim.openstreetmap.org/search?format=json&q={}",
+        city
+    );
+    let request = client
+        .get(url)
+        .header("User-Agent", "reqwest")
+        .header("Accept", "application/json")
+        .send()
+        .await
+        .expect("Failed to fetch nominatim");
+
+    let json_value = request
+        .json::<serde_json::Value>()
+        .await
+        .expect("Failed to parse JSON");
+
+    if let Some(json_array) = json_value.as_array() {
+        let cities: Vec<HashMap<&str, Value>> = json_array
+            .iter()
+            .filter(|row| {
+                row.get("osm_type")
+                    .map_or(false, |osm_type| osm_type == "relation")
+            })
+            .map(|row| {
+                let mut map: HashMap<&str, Value> = HashMap::new();
+                map.insert("key", json!(city));
+                map.insert("name", row.get("display_name").unwrap().clone());
+                map.insert("type", row.get("type").unwrap().clone());
+                map.insert(
+                    "area_id",
+                    json!(row.get("osm_id").unwrap().as_f64().unwrap() + 36e8),
+                );
+                return map;
+            })
+            .collect();
+        // throw if no city found
+        if cities.len() == 0 {
+            return Err(());
+        }
+        return Ok(cities);
+    }
+    return Err(());
+}
+
+pub async fn execute_osm_query(query: &str) -> Value {
+    let client = reqwest::Client::new();
+
+    println!("{}", query);
+
+    let request = client
+        .get("https://overpass-api.de/api/interpreter")
+        .body(format!("data={}", query))
+        .header("User-Agent", "reqwest")
+        .header("Accept", "application/json")
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .send()
+        .await
+        .expect("Failed to fetch overpass query");
+
+    let json_value = request
+        .json::<serde_json::Value>()
+        .await
+        .expect("Failed to parse JSON");
+
+    return json_value;
+}
+
+pub fn create_folder_at_path(path: &str) {
+    std::fs::create_dir_all(path).expect("Failed to create folder");
+}
+
+pub fn check_for_file(path: &str) -> bool {
+    return std::path::Path::new(path).exists();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,52 @@
-use reqwest;
-use serde_json::{json, Value};
-use std::collections::HashMap;
+use a_star_wallpaper::{check_for_file, create_folder_at_path, execute_osm_query, grab_city_data};
+use serde_json::Value;
+use std::{collections::HashMap, fs::File, io::BufWriter};
 
-async fn grab_city_data(city: &str) -> Result<Vec<HashMap<&str, Value>>, ()> {
-    let client = reqwest::Client::new();
-    let url = format!(
-        "https://nominatim.openstreetmap.org/search?format=json&q={}",
-        city
-    );
-    let request = client
-        .get(url)
-        .header("User-Agent", "reqwest")
-        .header("Accept", "application/json")
-        .send()
-        .await
-        .expect("Failed to fetch");
-
-    let json_value = request
-        .json::<serde_json::Value>()
-        .await
-        .expect("Failed to parse JSON");
-
-    if let Some(json_array) = json_value.as_array() {
-        let cities: Vec<HashMap<&str, Value>> = json_array
-            .iter()
-            .filter(|row| {
-                row.get("osm_type")
-                    .map_or(false, |osm_type| osm_type == "relation")
-            })
-            .map(|row| {
-                let mut map: HashMap<&str, Value> = HashMap::new();
-                map.insert("key", json!(city));
-                map.insert("name", row.get("display_name").unwrap().clone());
-                map.insert("type", row.get("type").unwrap().clone());
-                map.insert(
-                    "areaId",
-                    json!(row.get("osm_id").unwrap().as_f64().unwrap() + 36e8),
-                );
-                return map;
-            })
-            .collect();
-        // throw if no city found
-        if cities.len() == 0 {
-            return Err(());
-        }
-        return Ok(cities);
-    }
-    return Err(());
+fn get_query(area_id: &str) -> String {
+    return format!("[timeout:900][out:json];\
+        area({});\
+        (._; )->.area;\
+        (\
+        way[highway~'^(((motorway|trunk|primary|secondary|tertiary)(_link)?)|unclassified|residential|living_street|service|track)$'](area.area);\
+        node(w);\
+        );\
+        out skel;", area_id);
 }
 
-#[tokio::main]
-async fn main() {
+fn save_file(path_data: Value, path_name: &str) -> Result<(), ()> {
+    let file = File::create(path_name).expect("Failed to create file");
+    let mut writer = BufWriter::new(file);
+    serde_json::to_writer_pretty(&mut writer, &path_data).expect("Failed to write file");
+    Ok(())
+}
+
+async fn query_overpass(place: HashMap<&str, Value>) {
+    let area_id = place.get("area_id").unwrap().as_f64().unwrap().to_string();
+    let city_name = place.get("key").unwrap().as_str().unwrap();
+    let query = get_query(&area_id);
+    create_folder_at_path("tmp");
+    let path = format!("tmp/{}_{}.json", city_name, area_id);
+    if check_for_file(path.as_str()) {
+        println!("File {} already exists", path);
+        return;
+    }
+    let query_result = execute_osm_query(&query).await;
+    save_file(query_result, &path).expect("Failed to save file");
+}
+
+async fn grab_cities() {
     let city = "Vancouver";
     let city_data = grab_city_data(city)
         .await
         .expect("Failed to grab city data");
-    println!("{:?}", city_data);
+
+    // loop over run async
+    for place in city_data {
+        query_overpass(place).await;
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    grab_cities().await;
 }
